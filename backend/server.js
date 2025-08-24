72beeaf39b2d8048d4cb33070184a04e3e842b55
psharathchandra <psharathchandra984@gmail.com>
2025-08-24 00:00:52 -0400

    fixes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const bodyParser = require("body-parser"); /* deprecated */
 const cors = require("cors");
-
+require("dotenv").config(); 
 const app = express();
 
 app.use(cors());
