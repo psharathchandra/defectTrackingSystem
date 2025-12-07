83adfb7b996e1e94e4d78fb27a4ede49afe647ad
psharathchandra <psharathchandra984@gmail.com>
2025-12-07 18:14:44 -0500

    env issue fixed

--- frontend/src/configLoader.js
+++ frontend/src/configLoader.js
@@ -3,21 +3,21 @@ import staticConfig from './config.json';
 
 const configLoader = () => {
   return {
-    SERVER_URL: process.env.SERVER_URL || staticConfig.SERVER_URL || 'http://localhost:8080/api/',
-    BE_SERVER_URL: process.env.SERVER_URL || staticConfig.BE_SERVER_URL || 'http://localhost:8080/api/',
-    USERS_TABLE_LENGTH: parseInt(process.env.USERS_TABLE_LENGTH) || staticConfig.USERS_TABLE_LENGTH || 2,
-    BUGS_TABLE_LENGTH: parseInt(process.env.BUGS_TABLE_LENGTH) || staticConfig.BUGS_TABLE_LENGTH || 2,
-    REPORTS_PAGE_TITLE: process.env.REPORTS_PAGE_TITLE || staticConfig.REPORTS_PAGE_TITLE,
-    PROJECTS_PAGE_TITLE: process.env.PROJECTS_PAGE_TITLE || staticConfig.PROJECTS_PAGE_TITLE,
-    DEFECTS_PAGE_TITLE: process.env.DEFECTS_PAGE_TITLE || staticConfig.DEFECTS_PAGE_TITLE,
-    USERS_PAGE_TITLE: process.env.USERS_PAGE_TITLE || staticConfig.USERS_PAGE_TITLE,
-    APP_TITLE: process.env.APP_TITLE || staticConfig.APP_TITLE,
-    USER_LINK_NAME: process.env.USER_LINK_NAME || staticConfig.USER_LINK_NAME,
-    PROJECT_LINK_NAME: process.env.PROJECT_LINK_NAME || staticConfig.PROJECT_LINK_NAME,
-    DEFECTS_LINK_NAME: process.env.DEFECTS_LINK_NAME || staticConfig.DEFECTS_LINK_NAME,
-    REPORTS_LINK_NAME: process.env.REPORTS_LINK_NAME || staticConfig.REPORTS_LINK_NAME,
+    SERVER_URL: process.env.REACT_APP_SERVER_URL || staticConfig.SERVER_URL || 'http://localhost:8080/api/',
+    BE_SERVER_URL: process.env.REACT_APP_SERVER_URL || staticConfig.BE_SERVER_URL || 'http://localhost:8080/api/',
+    USERS_TABLE_LENGTH: parseInt(process.env.REACT_APP_USERS_TABLE_LENGTH) || staticConfig.USERS_TABLE_LENGTH || 2,
+    BUGS_TABLE_LENGTH: parseInt(process.env.REACT_APP_BUGS_TABLE_LENGTH) || staticConfig.BUGS_TABLE_LENGTH || 2,
+    REPORTS_PAGE_TITLE: process.env.REACT_APP_REPORTS_PAGE_TITLE || staticConfig.REPORTS_PAGE_TITLE,
+    PROJECTS_PAGE_TITLE: process.env.REACT_APP_PROJECTS_PAGE_TITLE || staticConfig.PROJECTS_PAGE_TITLE,
+    DEFECTS_PAGE_TITLE: process.env.REACT_APP_DEFECTS_PAGE_TITLE || staticConfig.DEFECTS_PAGE_TITLE,
+    USERS_PAGE_TITLE: process.env.REACT_APP_USERS_PAGE_TITLE || staticConfig.USERS_PAGE_TITLE,
+    APP_TITLE: process.env.REACT_APP_APP_TITLE || staticConfig.APP_TITLE,
+    USER_LINK_NAME: process.env.REACT_APP_USER_LINK_NAME || staticConfig.USER_LINK_NAME,
+    PROJECT_LINK_NAME: process.env.REACT_APP_PROJECT_LINK_NAME || staticConfig.PROJECT_LINK_NAME,
+    DEFECTS_LINK_NAME: process.env.REACT_APP_DEFECTS_LINK_NAME || staticConfig.DEFECTS_LINK_NAME,
+    REPORTS_LINK_NAME: process.env.REACT_APP_REPORTS_LINK_NAME || staticConfig.REPORTS_LINK_NAME,
     DATE_FORMAT_OBJECT: staticConfig.DATE_FORMAT_OBJECT,
-    DATE_REGION: process.env.DATE_REGION || staticConfig.DATE_REGION,
+    DATE_REGION: process.env.REACT_APP_DATE_REGION || staticConfig.DATE_REGION,
   };
 };
 
